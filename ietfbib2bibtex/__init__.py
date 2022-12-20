cdd4b6f10cfa5989f0fbe83a4d4ff487e162fdf5
Martine Lenders <m.lenders@fu-berlin.de>
2022-12-20 16:14:18 +0100

    Distify

---
+++ ietfbib2bibtex/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2022 Freie Universität Berlin
+#
+# This file is subject to the terms and conditions of the GNU Lesser
+# General Public License v2.1. See the file LICENSE in the top level
+# directory for more details.
+
+__author__ = "Martine S. Lenders"
+__copyright__ = "Copyright 2022 Freie Universität Berlin"
+__license__ = "LGPL v2.1"
+__email__ = "m.lenders@fu-berlin.de"
+__version__ = "v0.9.0"
---
+++ setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import os
+from setuptools import setup, find_packages
+
+PACKAGE = "ietfbib2bibtex"
+LICENSE = "MIT"
+URL = "https://github.com/TBD/ietfbib2bibtex"
+
+
+def get_requirements():
+    with open("requirements.txt", encoding="utf-8") as req_file:
+        for line in req_file:
+            yield line.strip()
+
+
+def get_version(package):
+    """Extract package version without importing file
+    Importing cause issues with coverage,
+        (modules can be removed from sys.modules to prevent this)
+    Importing __init__.py triggers importing rest and then requests too
+
+    Inspired from pep8 setup.py
+    """
+    with open(os.path.join(package, "__init__.py"), encoding="utf-8") as init_fd:
+        for line in init_fd:
+            if line.startswith("__version__"):
+                return eval(line.split("=")[-1])  # pylint:disable=eval-used
+    return None
+
+
+setup(
+    name=PACKAGE,
+    version=get_version(PACKAGE),
+    description=(
+        "ietfbib2bibtex - "
+        "A tool to generate bibtex bibliographies from IETF bibliographies"
+    ),
+    # long_description=open("README.rst").read(),
+    # long_description_content_type="text/x-rst",
+    author="Martine S. Lenders",
+    author_email="m.lenders@fu-berlin.de",
+    url=URL,
+    license=LICENSE,
+    download_url=URL,
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Science/Research",
+        "Environment :: Console",
+        "Topic :: Utilities",
+    ],
+    install_requires=list(get_requirements()),
+    entry_points={
+        "console_scripts": [
+            "ietfbib2bibtex = ietfbib2bibtex.cli:main",
+        ],
+    },
+    python_requires=">=3.7",
+)
