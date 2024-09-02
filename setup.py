93387963c4f43407814e4d69c6383de257ad1bd1
Martine Lenders <m.lenders@fu-berlin.de>
2024-09-02 18:58:54 +0200

    Add Python 3.12 support

--- setup.py
+++ setup.py
@@ -52,6 +52,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Intended Audience :: End Users/Desktop",
         "Intended Audience :: Science/Research",
         "Environment :: Console",
